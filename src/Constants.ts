a1d35cfc3c17e4238230daff361c8eb10e442235
Popov72 <github@evpopov.com>
2019-10-18 23:55:12 +0200

    Allow to modify the ObjectID object

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,7 +2,7 @@ export const baseFrameRate = 30.0;
 export const uvRotateTileHeight = 64;
 export const moveableScrollAnimTileHeight = 128;
 
-export const ObjectID = {
+export const ObjectID: any = {
     "Lara":             0,
     "LaraJoints":       9,
 };
