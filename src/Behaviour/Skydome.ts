ae1e6ddde08324bc0742c2e0eeada523e45a407d
Popov72 <github@evpopov.com>
2019-10-21 20:09:15 +0200

    Add the Skydome behaviour back

---
+++ src/Behaviour/Skydome.ts
@@ -0,0 +1,104 @@
+import Engine from "../Proxy/Engine";
+import { ICamera } from "../Proxy/ICamera";
+import { IMesh } from "../Proxy/IMesh";
+import IGameData from "../Player/IGameData";
+import { Behaviour, BehaviourRetCode } from "./Behaviour";
+import { BehaviourManager } from "./BehaviourManager";
+import { ObjectManager } from "../Player/ObjectManager";
+import SkyDome from "../Utils/SkyDome";
+
+export class Skydome extends Behaviour {
+
+    public name: string = Skydome.name;
+
+    protected objSky: IMesh;
+    protected camera: ICamera;
+    protected objMgr: ObjectManager;
+
+    constructor(nbhv: any, gameData: IGameData, objectid?: number, objecttype?: string) {
+        super(nbhv, gameData, objectid, objecttype);
+
+        this.objMgr = gameData.objMgr;
+        this.camera = gameData.camera;
+        this.objSky = <any>null;
+
+        this.createSkyDome();
+    }
+
+    public init(lstObjs: Array<IMesh | ICamera> | null): [BehaviourRetCode, Array<Promise<void>> | null] {
+        const objSky = this.objMgr.objectList['skydome'];
+
+        if (!objSky || !objSky[0]) {
+            return [BehaviourRetCode.dontKeepBehaviour, null];
+        }
+
+        this.objSky = (objSky[0] as Array<IMesh>)[0];
+
+        this.objSky.renderOrder = 0;
+        this.objSky.matrixAutoUpdate = true;
+        
+        this.gameData.sceneBackground.add(this.objSky);
+
+        const skyColor = [this.nbhv.color.r/255.0, this.nbhv.color.g/255.0, this.nbhv.color.b/255.0],
+              material = this.objSky.materials[0];
+
+        material.depthWrite = false;
+        material.uniforms.tintColor.value = skyColor;
+
+        material.uniformsUpdated();
+
+        return [BehaviourRetCode.keepBehaviour, null];
+    }
+
+    public frameEnded(curTime: number, delta: number): void {
+        this.objSky.setPosition(this.camera.position);
+
+        const material = this.objSky.materials[0];
+
+        let pgr = curTime / 50.0;
+        
+        material.uniforms.offsetRepeat.value[0] = pgr - Math.floor(pgr);
+
+        material.uniformsUpdated(["offsetRepeat"]);
+    }
+
+    protected createSkyDome(): void {
+        const meshData = SkyDome.create(
+            /*curvature*/ 10.0,
+            /*tiling*/ 3,
+            /*distance*/ 2000.0,
+            /*orientation*/ [0,0,0,1],
+            /*xsegments*/ 16, 
+            /*ysegments*/ 16,
+            /*ySegmentsToKeep*/ 8
+        );
+
+        const skyTexture = this.gameData.sceneData.textures[this.gameData.sceneData.textures.length-1];
+
+        (skyTexture as any).wrapS = (skyTexture as any).wrapT = 1000; // for threejs
+        (skyTexture as any).wrapU = (skyTexture as any).wrapV = 1; // for babylon - don't want to abstract that...
+
+        const uniforms = {
+            "map" :             { type: "t",    value: skyTexture },
+            "offsetRepeat" :    { type: "f4",   value: [0, 0, 1, 1] },
+            "tintColor" :       { type: "f3",   value: [0, 0, 0] }
+        };
+
+        const meshb = Engine.makeMeshBuilder(),
+              sky = meshb.createMesh('skydome', this.gameData.shdMgr.getVertexShader('skydome'), this.gameData.shdMgr.getFragmentShader('skydome'), uniforms, meshData.vertices, meshData.faces, meshData.textures, undefined);
+
+        this.gameData.sceneData.objects['skydome'] = {
+            "type"					: 'skydome',
+            "objectid"              : '0',
+            "roomIndex"             : -1,
+            "visible"  				: true
+        };
+
+        this.objMgr.objectList['skydome'] = {0: [sky] };
+    }
+
+}
+
+BehaviourManager.registerFactory(Skydome.name, 
+    (nbhv: any, gameData: any, objectid?: number, objecttype?: string) => new Skydome(nbhv, gameData, objectid, objecttype)
+);
--- threeJS/src/Behaviours.ts
+++ threeJS/src/Behaviours.ts
@@ -9,6 +9,7 @@ import "../../src/Behaviour/RemoveObject";
 import "../../src/Behaviour/ScrollTexture";
 import "../../src/Behaviour/SetAnimation";
 import "../../src/Behaviour/Sky";
+import "../../src/Behaviour/Skydome";
 import "../../src/Behaviour/Sprite";
 import "../../src/Behaviour/UVRotate";
 import "../../src/Behaviour/Zbias";
