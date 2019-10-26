2ede910522b48f41895b939df3f94c0d13080b87
Popov72 <github@evpopov.com>
2019-10-26 18:11:27 +0200

    Make sure entries in ObjectID are numbers

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,7 +2,8 @@ export const baseFrameRate = 30.0;
 export const uvRotateTileHeight = 64;
 export const moveableScrollAnimTileHeight = 128;
 
-export const ObjectID: any = {
+export const ObjectID: { [name: string]: number } = {
     "Lara":             0,
     "LaraJoints":       9,
+    "LaraBraid":        -1,
 };
